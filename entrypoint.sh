450895d2a92938589a2a141bd0b04de8d71d5fe3
Octavio <octabau02@outlook.com>
2026-06-13 18:20:25 -0600

    Agrega chown de storage/database en entrypoint para permisos de escritura

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 mkdir -p /var/www/html/storage/database
 php artisan package:discover --ansi
 php artisan migrate --force
+chown -R www-data:www-data /var/www/html/storage/database
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
